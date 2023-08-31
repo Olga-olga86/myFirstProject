718c5c57ddb3003f7a1ceed94f7e8eebdb089fce
Olga-olga86 <olyanovember@gmail.com>
2023-08-31 10:35:39 +0300

    HW2 is done.

---
+++ HomeWorks/java/lesson2/Task1HW2.java
@@ -0,0 +1,23 @@
+package lesson2;
+
+public class Task1HW2 {
+    public Task1HW2() {
+    }
+
+    static int sumOfDigits(int abc) {
+        int number = 123;
+        int sum = 0;
+        int currentDigit = number % 10;
+        sum += currentDigit;
+        number /= 10;
+        currentDigit = number % 10;
+        sum += currentDigit;
+        number /= 10;
+        sum += number;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(123));
+    }
+}
