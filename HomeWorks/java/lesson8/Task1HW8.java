fc0a8c24929949d5073e167f5b2a0cbe72af0f23
Olga-olga86 <olyanovember@gmail.com>
2023-09-21 10:37:39 +0300

    HW6 is done.

---
+++ HomeWorks/java/lesson8/Task1HW8.java
@@ -0,0 +1,10 @@
+package lesson8;
+
+public class Task1HW8 {
+
+    //Top 4 most effective locators for web elements on GitHub's Code tab
+  String codeTab = "WebElement element = driver.findElement (By.id ("code-tab"))";
+  String goToFileButton = "WebElement element = driver.findElement (By.linkText ("Go to file"))";
+  String buttonNavigationMenu = "WebElement element = driver.findElement (By.id ("dialog-show-dialog-d92d47ee-e093-489f-99d5-cb8f6f2f5e82"))";
+  String GetPushNotificationsOn  = "WebElement element = driver.findElement (By.className ("text-small color-fg-muted text-normal pb-1"))";
+}
---
+++ HomeWorks/java/lesson8/Task2HW8.java
@@ -0,0 +1,12 @@
+package lesson8;
+
+public class Task2HW8 {
+
+    //5 xpath locators for web elements on GitHub's Actions tab
+
+    String actionsTab = "//*[@id='actions-tab']";
+    String searchPlaceholder = "//*[@id='placeholder']";
+    String navigationMenu = "//*[@id='dialog-show-dialog-0cbe30e0-ce7e-4941-9fc2-9547880a1167']";
+    String svgActionsTab = "//*[@id='actions-tab']/svg";
+    String setUpAWorkflowYourself = "//*[@id='repo-content-turbo-frame']/div/div/div/div/div/div[1]/div/div/p[2]/a";
+}
